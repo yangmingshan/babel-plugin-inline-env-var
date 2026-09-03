b7cd0d8216710975e0e326346f36d1eb784ac52e
Yang Mingshan <y.mingshan3@gmail.com>
2026-09-03 15:03:55 +0800

    refactor: replace process.env with env from node:process

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { env } from 'node:process'
+
 export default function inlineEnvVar({ types: t }) {
   const isLeftSideOfAssignmentExpression = (path) =>
     t.isAssignmentExpression(path.parent) && path.parent.left === path.node
@@ -14,7 +16,7 @@ export default function inlineEnvVar({ types: t }) {
             (!include || include.indexOf(key.value) !== -1) &&
             (!exclude || exclude.indexOf(key.value) === -1)
           ) {
-            path.replaceWith(t.valueToNode(process.env[key.value]))
+            path.replaceWith(t.valueToNode(env[key.value]))
           }
         }
       },
